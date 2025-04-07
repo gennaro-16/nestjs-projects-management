a1d99d5664187efd38ae860727e563bddf13681e
genny16 <varaforskare@gmail.com>
2025-04-07 15:09:16 +0100

    verfi email with 6 digits

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // All standard HTTP methods
+    allowedHeaders: '*', // Accept all headers
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3001);
   
