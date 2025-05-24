a0b926aea81cd35231def23da1914945009bfa27
genny16 <varaforskare@gmail.com>
2025-05-24 17:20:08 +0100

    project id comes in login

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-
 async function bootstrap() {
-  
   const app = await NestFactory.create(AppModule);
+  
+  // Ultra-permissive CORS (allows everything)
   app.enableCors({
-    origin: '*', // Allow all origins
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // All standard HTTP methods
-    allowedHeaders: '*', // Accept all headers
+    origin: '*',                 // Allow ALL origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // All HTTP methods
+    allowedHeaders: '*',         // Allow ALL headers
+    credentials: false,          // Disable credentials (set to `true` if using cookies/auth)
   });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3001);
-  
 }
 bootstrap();
